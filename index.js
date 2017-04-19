fabbe47c8638b708de9871cb0659386795445e6b
Orhan <nahro.tozan@gmail.com>
2017-04-19 19:20:04 +0200

    first changes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-index.js
+$(document).ready(function()
+{
+	var canvas = new fabric.Canvas('c');
+	fabric.Image.fromURL('part3.png', function(oImg)
+	{
+		oImg.selectable = false;
+		oImg.scale(0.75);
+		canvas.add(oImg);
+	});
+});
